31f1b49a1067b6193e5a6bd3141798f8cc093fc4
ginx836 <141227582+ginx836@users.noreply.github.com>
2024-01-16 09:34:30 +0100

    Update Projects.jsx, Add Carousel.jsx
    Mise à jour du composant Projects.jsx (+styles)
    Création du composant Carousel pour la présentation des projets
    
    Update Styles

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,49 @@
-import './projects.scss' 
+import Carousel from '../utils/Carousel/Carousel'
+import './projects.scss'
 
 const Projects = () => {
+  const data = [
+    {
+      id: 0,
+      header: 'Projet 1',
+      body: 'Chillwave knausgaard chambray flannel tumblr, narwhal microdosing blog...',
+      colour: '#242846',
+      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/735173/rvc1.jpg',
+    },
+    {
+      id: 1,
+      header: 'Projet 2',
+      body: 'Swag biodiesel disrupt retro fashion, salvia food truck kitsch wolf DIY...',
+      colour: '#ba9077',
+      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/735173/rvc2.jpg',
+    },
+    {
+      id: 2,
+      header: 'Proejt 3',
+      body: 'Flexitarian 3 wolf moon cliche, migas scenester street art...',
+      colour: '#1ABC9C',
+      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/735173/rvc3.jpg',
+    },
+    {
+      id: 3,
+      header: 'Projet 4',
+      body: 'Farm-to-table tousled try-hard, normcore ethical tilde iPhone...',
+      colour: '#C0392B',
+      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/735173/rvc4.jpg',
+    },
+    {
+      id: 4,
+      header: 'Projet 5',
+      body: 'Tote bag flannel normcore polaroid +1. Quinoa actually 90s sustainable...',
+      colour: '#513B56',
+      img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/735173/rvc5.jpg',
+    },
+  ]
+
   return (
     <div className="projects__wrapper">
-      <h2>Mes projets</h2>
+      <h2 className="projects__title">Mes projets</h2>
+      <Carousel data={data} />
     </div>
   )
 }
---
+++ src/components/utils/Carousel/Carousel.jsx
@@ -0,0 +1,65 @@
+import './carousel.scss'
+import { useState } from 'react'
+
+const Carousel = ({ data }) => {
+  const [activeID, setActiveID] = useState(0)
+  const [buttonHover, setButtonHover] = useState(false)
+
+  const activeData = data[activeID]
+
+  const buttonStyle = {
+    color: buttonHover ? activeData.colour : '#ffffff',
+  }
+
+  return (
+    <section
+      className="wrapper"
+      style={{ backgroundImage: `url('${activeData.img}')` }}
+    >
+      <Selectors data={data} activeID={activeID} changeActive={setActiveID} />
+      <Panel
+        data={activeData}
+        panelStyle={{ backgroundColor: activeData.colour }}
+        buttonStyle={buttonStyle}
+        toggleButtonColour={() => setButtonHover(!buttonHover)}
+      />
+    </section>
+  )
+}
+
+const Panel = ({ data, panelStyle, buttonStyle, toggleButtonColour }) => (
+  <aside className="panel" style={panelStyle}>
+    <h2 className="panel-header">{data.header}</h2>
+    <p className="panel-info">{data.body}</p>
+    <button
+      className="panel-button"
+      style={buttonStyle}
+      onMouseEnter={toggleButtonColour}
+      onMouseLeave={toggleButtonColour}
+    >
+      Read More
+    </button>
+  </aside>
+)
+
+const Selectors = ({ data, activeID, changeActive }) => (
+  <div className="selectors">
+    {data.map((item) => (
+      <Selector
+        key={item.id}
+        id={item.id}
+        handleClick={() => changeActive(item.id)}
+        isActive={activeID === item.id}
+      />
+    ))}
+  </div>
+)
+
+const Selector = ({ handleClick, isActive }) => (
+  <div
+    className={`selector ${isActive ? 'active' : ''}`}
+    onClick={handleClick}
+  ></div>
+)
+
+export default Carousel

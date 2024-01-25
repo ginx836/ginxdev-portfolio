9d3255b57865533ad51a32b913c6a7f6fd7eaf06
ginx836 <141227582+ginx836@users.noreply.github.com>
2024-01-25 14:23:49 +0100

    Update socials at Hero.jsx
    Mise à jour des réseaux sur le Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,9 @@
+import { LinkedinPlain, GithubOriginal } from 'devicons-react'
+
 import PropTypes from 'prop-types'
 import './hero.scss'
 
-
-const Hero = ({ title, text, picture, subtitle, subtitle2}) => {
+const Hero = ({ title, text, picture, subtitle, subtitle2 }) => {
   return (
     <div className="hero">
       <img className="hero__picture" src={picture} alt="Avatar" />
@@ -11,6 +12,22 @@ const Hero = ({ title, text, picture, subtitle, subtitle2}) => {
         <p className="hero__text">{text}</p>
         <p className="hero__subtitle">{subtitle}</p>
         <p className="hero__subtitle2">{subtitle2}</p>
+        <div className="hero__social">
+          <a
+            href="https://www.linkedin.com/in/cedric-bourquin-092a8a250/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedinPlain color="white" size={60} />
+          </a>
+          <a
+            href="https://github.com/ginx836"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GithubOriginal style={{ filter: 'invert(100%)' }} size={60} />
+          </a>
+        </div>
       </section>
     </div>
   )
